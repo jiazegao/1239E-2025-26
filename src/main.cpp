1a626a898a9d81aa71b676102191c890770b4732
Ivan <122951356+equinochrome@users.noreply.github.com>
2026-01-16 18:13:43 -0600

    awdkawdwakdakwawd

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,6 @@
 #include "custom/util_funcs.h"
 #include "custom/auton_selector.h" // IWYU pragma: keep
 #include "pros/motors.h"
-#include "pros/motors.h"
 
 void initialize() {
     chassis.calibrate();
@@ -18,19 +17,28 @@ void disabled() {}
 void competition_initialize() {}
 
 void autonomous() {
-	chassis.setBrakeMode(pros::E_MOTOR_BRAKE_HOLD);
+	startControllerDisplay();
+	chassis.setBrakeMode(pros::E_MOTOR_BRAKE_COAST);
 	chassis.setPose(0, 0, 0);
-	chassis.moveToPoint(0, 	170, 99999, {}); // Brief delay to ensure everything is initialized
+	//1
+	while (true) {
+	chassis.moveToPoint(0, 	190, 99999, {}); // Brief delay to ensure everything is initialized
 	//chassis.turnToPoint(114, 114, 9999);
 	chassis.waitUntilDone();
-	chassis.turnToHeading(90, 600);
-	chassis.moveToPoint(170, 170, 90999);
+	chassis.turnToHeading(90, 600, { .minSpeed=50}, false);
+	chassis.moveToPoint(305, 195, 9999);
 	chassis.waitUntilDone();
 	chassis.turnToHeading(180, 400);
-	chassis.moveToPoint(170, -170, 90999);
+	//3
+	chassis.moveToPoint(300, -130, 90999);
 	chassis.waitUntilDone();
+	//4
 	chassis.turnToHeading(270, 400);
-	chassis.moveToPoint(-170, -170, 99999);
+	chassis.moveToPoint(-0, -140, 99999);
+	//5
+	chassis.turnToHeading(0, 400);
+	chassis.moveToPoint(0, 0, 99999);
+	}
 }
 
 void opcontrol() {
