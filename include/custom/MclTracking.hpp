fa4b0f7fbf79d7e4c472936357c64658a6459ea8
Jason <jiaze.gao@outlook.com>
2026-02-14 21:08:32 -0600

    Update 3 2/14/2026

--- include/custom/MclTracking.hpp
+++ include/custom/MclTracking.hpp
@@ -10,25 +10,25 @@
 #include "pros/rotation.hpp"
 
 // --- Configuration Constants ---
-const int PARTICLE_COUNT = 1200;
-const int RESAMPLE_THRESHOLD = 80;
-const int LOG_AMOUNT = 25;
+const int PARTICLE_COUNT = 1000;
+const int RESAMPLE_THRESHOLD = 100;
+const int LOG_AMOUNT = 20;
 const int LOG_RATIO = PARTICLE_COUNT / LOG_AMOUNT;
 
 const double MAX_RANGE = 300.0;
-const double BASE_DIST_SIGMA_L787 = 0.5;    // 0 ~ 200 mm
-const double BASE_DIST_SIGMA_G787 = 2.0;    // > 200 mm
+const double BASE_DIST_SIGMA_L787 = 0.3;    // 0 ~ 200 mm
+const double BASE_DIST_SIGMA_G787 = 1.5;    // > 200 mm
 const double HEADING_SIGMA = 0.05;
 const double DIST_RESAMPLE_VARIANCE = 1.5;
-const double THETA_RESAMPLE_VARIANCE = 0.03;
+const double THETA_RESAMPLE_VARIANCE = 0.02;
 const int CONFIDENCE_THRESHOLD = 40;
-const double TRACKING_WHEEL_VARIANCE = 0.06;
+const double TRACKING_WHEEL_VARIANCE = 0.02;
 const double FAULT_TOLERANCE = 1e-300;
-const double UNCERTAINTY_TOLERANCE = 1.0;
+const double UNCERTAINTY_TOLERANCE = 0.8;
 const double DIST_SYNC_PROP = 0.1;
 const double THETA_SYNC_PROP = 0.002;
 
-const double MSPT = 25;
+const double MSPT = 20;
 const double MINPAUSE = 10;
 
 struct Pose { double x, y, theta; };
