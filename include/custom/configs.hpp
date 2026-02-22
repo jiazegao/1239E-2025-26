48e1eecaa54c921fa650cd16f86bdbb975fe7bbc
Ivan <122951356+equinochrome@users.noreply.github.com>
2026-02-22 17:00:39 -0600

    Update 3 2/22/2026
    Co-authored-by: Jason G <jiazegao@users.noreply.github.com>

--- include/custom/configs.hpp
+++ include/custom/configs.hpp
@@ -26,14 +26,14 @@ inline alliance_color allianceColor = alliance_color::BLUE;
 inline pros::Controller controller(pros::E_CONTROLLER_MASTER);
 
 // Motors
-inline pros::MotorGroup leftMotors({-11, 12, -13}, pros::MotorGearset::blue);
-inline pros::MotorGroup rightMotors({20, -19, 18}, pros::MotorGearset::blue);
+inline pros::MotorGroup leftMotors({-11, -12, 13}, pros::MotorGearset::blue);
+inline pros::MotorGroup rightMotors({20, 19, -18}, pros::MotorGearset::blue);
 inline pros::Motor frontMotor(17, pros::MotorGearset::blue);
 inline pros::Motor leverMotor(-16, pros::MotorGearset::red);
 
 inline lemlib::Drivetrain drivetrain(&leftMotors,
                               &rightMotors,
-                              11.375,
+                              10.400,
                               3.25,
                               450,
                               2
